0d1c64c3dcaf414c8c5c1e36f637d28b18deb2e7
jlkansakar <joka0002@stud.kea.dk>
2024-02-22 23:47:01 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "kotlin_handin_03"
+include(":app")
+ 
