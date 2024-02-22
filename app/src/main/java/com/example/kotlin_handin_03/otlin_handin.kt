a7645b28b2c7afefbd73a373e7129d473de52d37
jlkansakar <joka0002@stud.kea.dk>
2024-02-23 00:00:09 +0100

    added kt file

---
+++ app/src/main/java/com/example/kotlin_handin_03/otlin_handin.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin_handin_03
+
+fun main() {
+
+}
+
+class Article(author: String, title: String){
+    override fun toString(): String {
+        println()
+        return "printedaaa"
+    }
+}
